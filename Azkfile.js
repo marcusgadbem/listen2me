1a516f1d4acbefdb1f8410e3833441fd9adb88cc
Marcus Gadbem <mcsgad@gmail.com>
2015-11-08 16:16:33 -0200

    Updated Azkfile: node system, removed command from mongodb

--- Azkfile.js
+++ Azkfile.js
@@ -1,25 +1,17 @@
-/**
- * Documentation: http://docs.azk.io/Azkfile.js
- */
-
-// Adds the systems that shape your system
 systems({
-
-  'l2m': {
-    // Dependent systems
+  'listen2me': {
     depends: ['mongodb'],
-    // More images:  http://images.azk.io
-    image: {"docker": "azukiapp/node:0.12"},
-    // Steps to execute before running instances
+    image: {"docker": "azukiapp/node"},
     provision: [
       "npm install"
     ],
     workdir: "/azk/#{manifest.dir}",
     shell: "/bin/bash",
     command: "npm start",
+    wait: {'retry': 20, 'timeout': 3000},
     mounts: {
       '/azk/#{manifest.dir}': path("."),
-      '/azk/#{manifest.dir}/bower_components': path("bower_components"),
+      '/azk/#{manifest.dir}/bower_modules': path("bower_modules"),
       '/azk/#{manifest.dir}/node_modules': path("node_modules")
     },
     scalable: {"default": 1},
@@ -31,39 +23,16 @@ systems({
     },
     envs: {
       NODE_ENV: "dev",
-      NODE_IP: "0.0.0.0",
       PORT: "8000",
-      APP_NAME: 'Listen 2 me!',
       APP_URL: "http://#{system.name}.#{azk.default_domain}",
-      SESSION_SECRET: 'brown sugar',
-      SERVICES: {
-        FACEBOOK: {
-          clientID: "750620371619521",
-          clientSecret: "47b7fb5ae41bb139f5839fe6fad83443",
-          callbackURL: "/auth/facebook/callback"
-        },
-        TWITTER: {
-          clientID: "vOhPoMCF2vrwpjtWHudcA",
-          clientSecret: "kxHKSMN403A2ETJL1HtTWipvPa8wsKUy1PiFztsqE",
-          callbackURL: "/auth/twitter/callback"
-        },
-        GOOGLE: {
-          clientID: "432822864473-2veuhlea45646s6s0joi82di0fkal9il.apps.googleusercontent.com",
-          clientSecret: "VMjB1WeaETl8r_CPkMTwYg_L",
-          callbackURL: "/auth/google/callback"
-        }
-      }
     },
   },
 
   mongodb: {
     image : { docker: "azukiapp/mongodb" },
-    command: 'mongod --rest --httpinterface',
     scalable: false,
     wait: {"retry": 20, "timeout": 1000},
-    // Mounts folders to assigned paths
     mounts: {
-      // equivalent persistent_folders
       '/data/db': persistent('mongodb-#{manifest.dir}'),
     },
     ports: {
@@ -76,7 +45,6 @@ systems({
       MONGODB_URI: "mongodb://#{net.host}:#{net.port[27017]}/#{manifest.dir}_development",
     },
   }
-
 });
 
 
